d7f3df5f4453bb5693eafac72182b49314bcdb46
Roman Putkov <putkr@amazon.com>
2023-04-05 23:06:49 +0200

    Ternary operator practice

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -161,3 +161,19 @@ if (day === 'monday'){
     console.log('Do you see what are you typing? Really?')
 }
  */
+const age = 28;
+// age >=18 ? console.log('I like to drink wine') :
+//     console.log('I like to drink water');
+
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
+
+let drink2;
+if (age >= 18){
+    drink2 = 'wine';
+} else {
+    drink2 = 'water';
+}
+console.log(drink2);
+
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
