d216171acba0f151ca43592370fff3214e0d8f00
Roman Putkov <putkr@amazon.com>
2023-04-05 21:47:30 +0200

    The switch Statement added

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -54,5 +54,21 @@ if(language === 'English' && population < 50 && isIsland !== true){
 
 switch (language) {
     case "chinese":
-    case "":
+    case "mandarin":
+        console.log('Most number of native speakers!');
+        break;
+    case 'spanish':
+        console.log('2nd place in number of native speakers');
+        break;
+    case 'english':
+        console.log('3rd place in number of native speakers');
+        break;
+    case 'hindi':
+        console.log('4th place in number of native speakers');
+        break;
+    case 'arabic':
+        console.log('5th place in number of native speakers');
+        break;
+    default:
+        console.log('Great language too :)');
 }
