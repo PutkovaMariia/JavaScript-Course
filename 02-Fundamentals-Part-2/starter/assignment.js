b410c68b2cfc0c61d234b4405704ee44e657389b
PutkovaMariia <124445804+PutkovaMariia@users.noreply.github.com>
2023-04-12 17:16:17 +0200

    Introduction to Arrays added

--- 02-Fundamentals-Part-2/starter/assignment.js
+++ 02-Fundamentals-Part-2/starter/assignment.js
@@ -1,16 +1,19 @@
 'use strict';
-function describeCountry(country, population, capitalCity){
+
+function describeCountry(country, population, capitalCity) {
     return `${country} has ${population} million people and its capital city is ${capitalCity}.`;
 }
+
 const ukraineDescription = describeCountry('Ukraine', 40, 'Kyiv');
 const polandDescription = describeCountry('Poland', 38, 'Warsaw');
 const greeceDescription = describeCountry('Greece', 10, 'Athens');
 
 console.log(ukraineDescription, polandDescription, greeceDescription);
 
-function percentageOfWorld1(population){
+function percentageOfWorld1(population) {
     return (population / 7900) * 100;
 }
+
 const perChina = `Chinese people in the world ${percentageOfWorld1(1441).toFixed(2)}% of population`;
 const perGreece = `Greek people in the world ${percentageOfWorld1(10).toFixed(2)}% of population`;
 const perPoland = `Polish people in the world ${percentageOfWorld1(38).toFixed(2)}% of population`;
@@ -18,7 +21,7 @@ console.log(perChina);
 console.log(perGreece);
 console.log(perPoland);
 
-const percentageOfWorld2 = function(population){
+const percentageOfWorld2 = function (population) {
     return (population / 7900) * 100;
 }
 const perChina2 = percentageOfWorld2(1441).toFixed(2);
@@ -32,9 +35,16 @@ const perGreece3 = percentageOfWorld3(10).toFixed(2);
 const perPoland3 = percentageOfWorld3(38).toFixed(2);
 console.log('percentageOfWorld3', perChina3, perGreece3, perPoland3);
 
-function describePopulation(country, population){
+function describePopulation(country, population) {
     return `${country} has ${population} million people, which is about ${percentageOfWorld3(population).toFixed(1)}% of the world.`;
 }
+
 console.log(describePopulation('China', 1441));
 console.log(describePopulation('Poland', 38));
 console.log(describePopulation('Greece', 10));
+
+const populations = [1441, 10, 38, 40];
+console.log(4 === populations.length);
+const percentages =
+    [percentageOfWorld1(populations[0]).toFixed(2), percentageOfWorld1(populations[1]).toFixed(2), percentageOfWorld1(populations[2]).toFixed(2), percentageOfWorld1(populations[3]).toFixed(2)];
+console.log(percentages);
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -44,7 +44,7 @@ const yearsUntilRetirement = (birthYear, firstName) => {
 console.log(yearsUntilRetirement(1994, 'Mariia'));
 console.log(yearsUntilRetirement(1992, 'Roman'));
 */
-
+/*
 function cutFruitPieces (fruit){
     return fruit * 3;
 }
@@ -54,3 +54,69 @@ function fruitProcessor(apples, oranges) {
     return `Juice with ${applePieces} pieces of apple and ${orangePieces} pieces of orange.`;
 }
 console.log(fruitProcessor(2, 3));
+*/
+/*
+const calcAge = birthYear => 2023 - birthYear;
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if(retirement > 0){
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired`);
+        return -1;
+    }
+}
+console.log(yearsUntilRetirement(1994, 'Masha'));
+console.log(yearsUntilRetirement(1950, 'Mike'));
+ */
+/*
+let counter = 0;
+
+function addFive() {
+    counter += 5
+    console.log(counter)
+}
+
+function addTwo() {
+    counter += 2
+    console.log(counter)
+}
+
+addFive(counter);// 5
+addTwo(counter);// 7
+ */
+const friend1 = 'Michael';
+const friend2 = 'Steven';
+const friend3 = 'Peter';
+
+const friends = ['Michael', 'Steven', 'Peter'];
+console.log(friends);
+
+const y = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+
+const firstName = 'Mariia';
+const mariia = [firstName, 'Putkova', 2023 - 1994, friends];
+console.log(mariia);
+/////
+const calcAge = birthYear => 2023 - birthYear;
+const years = [1990, 2018, 1977];
+
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
