1d008a54d9123c0374bc5d4d2bb7e2365f64657c
PutkovaMariia <124445804+PutkovaMariia@users.noreply.github.com>
2023-06-05 17:09:50 +0200

    for-of loop

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -45,6 +45,16 @@ const restaurant = {
     }
 };
 /*
+//for-of loop
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for (const item of menu) console.log(item);
+
+for (const [i, el] of menu.entries()){
+    console.log(`${i + 1}: ${el}`);
+}
+*/
+/*
 const rest1 = {
     name:'Capri',
     //numGuests: 20,
