cccfe806526c5515a8300a74796e81558976333e
PutkovaMariia <124445804+PutkovaMariia@users.noreply.github.com>
2023-07-18 16:14:58 +0200

    working with strings

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,8 +1,5 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights = '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 const weekdays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 const openingHours = {
     [weekdays[3]]: {
@@ -424,7 +421,7 @@ console.log(i, j, k);
 const [p=1, q=1, r=1] = [8, 9];
 console.log(p, q, r);
 */
-
+/*
 const airline = 'TAP Air Portugal';
 const plane = 'A320';
 
@@ -577,3 +574,19 @@ const planesInLine = function (n){
 }
 planesInLine(3);//There are 3 planes in line ✈✈✈
 planesInLine(12);//There are 12 planes in line ✈✈✈✈✈✈✈✈✈✈✈✈
+ */
+
+const flights = '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const getCode = str => str.slice(0,3).toUpperCase();
+
+for (const flight of flights.split('+')){
+    const [type, from, to, time] = flight.split(';');
+    const output =
+        `${type.startsWith('_Delayed') ? '🔴' : ''}${type.replaceAll('_', ' ')} from ${getCode(from)} to ${getCode(to)} (${time.replace(':', 'h')})`.padStart(45);
+    console.log(output);
+}
+// 🔴 Delayed Departure from FAO to TXL (11h25)
+//              Arrival from BRU to FAO (11h45)
+//   🔴 Delayed Arrival from HEL to FAO (12h05)
+//            Departure from FAO to LIS (12h30)
