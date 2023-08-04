f469088eaf3fa97b82c60b58b9b6e7c2eb246abd
PutkovaMariia <124445804+PutkovaMariia@users.noreply.github.com>
2023-08-04 13:22:56 +0200

    creating DOM elements

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -1,40 +1,38 @@
 'use strict';
 
-/////////////////////////////////////////////////
-/////////////////////////////////////////////////
 // BANKIST APP
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
-  interestRate: 1.2, // %
-  pin: 1111,
+    owner: 'Jonas Schmedtmann',
+    movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+    interestRate: 1.2, // %
+    pin: 1111,
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
-  movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
-  interestRate: 1.5,
-  pin: 2222,
+    owner: 'Jessica Davis',
+    movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
+    interestRate: 1.5,
+    pin: 2222,
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
-  interestRate: 0.7,
-  pin: 3333,
+    owner: 'Steven Thomas Williams',
+    movements: [200, -200, 340, -300, -20, 50, 400, -460],
+    interestRate: 0.7,
+    pin: 3333,
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
-  movements: [430, 1000, 700, 50, 90],
-  interestRate: 1,
-  pin: 4444,
+    owner: 'Sarah Smith',
+    movements: [430, 1000, 700, 50, 90],
+    interestRate: 1,
+    pin: 4444,
 };
 
 const accounts = [account1, account2, account3, account4];
-/*
+
 // Elements
 const labelWelcome = document.querySelector('.welcome');
 const labelDate = document.querySelector('.date');
@@ -60,7 +58,24 @@ const inputTransferAmount = document.querySelector('.form__input--amount');
 const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
-*/
+
+const displayMovements = function (movements) {
+    containerMovements.innerHTML = '';
+
+    movements.forEach(function (mov, i) {
+        const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+        const html = `
+        <div class="movements__row">
+          <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+          <div class="movements__value">${mov}</div>
+        </div>
+        `;
+
+        containerMovements.insertAdjacentHTML('afterbegin', html);
+    });
+}
+displayMovements(account1.movements);
 
 /////////////////////////////////////////////////
 /*
@@ -153,21 +168,22 @@ movements.forEach(function (mov, i, arr){//only in this order
   }
 });
 */
-
+/*
 //map
 const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
+    ['USD', 'United States dollar'],
+    ['EUR', 'Euro'],
+    ['GBP', 'Pound sterling'],
 ]);
 
-currencies.forEach(function (value, key, map){
-  console.log(`${key}: ${value}`);
+currencies.forEach(function (value, key, map) {
+    console.log(`${key}: ${value}`);
 })
 
 //set
-const currenciesUnique = new Set (['USD', 'GBR', 'USD', 'EUR', 'EUR']);
+const currenciesUnique = new Set(['USD', 'GBR', 'USD', 'EUR', 'EUR']);
 console.log(currenciesUnique);//Set(3) { 'USD', 'GBR', 'EUR' }
-currenciesUnique.forEach(function (value, _, map){//in sets we don`t have key
-  console.log(`${value}: ${value}`);
+currenciesUnique.forEach(function (value, _, map) {//in sets we don`t have key
+    console.log(`${value}: ${value}`);
 })
+ */
