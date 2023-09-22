1cded36c5a6aea62dde42879c1999fb005c1c0c1
PutkovaMariia <124445804+PutkovaMariia@users.noreply.github.com>
2023-09-22 16:01:28 +0200

    creating, inserting and deleting elements

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -8,7 +8,8 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
+const openModal = function (e) {
+  e.preventDefault();
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
@@ -18,8 +19,7 @@ const closeModal = function () {
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
@@ -29,3 +29,38 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+/////////////////////////////////////////////////
+
+//selecting elements
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+const header = document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+document.getElementById('section--1');
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);//if DOM changes - this collection also changes automatically
+
+console.log(document.getElementsByClassName('btn'));
+
+//creating and inserting elements
+const message = document.createElement('div');
+message.classList.add('cookie--message');
+// message.textContent = 'We use cookies for improved functionality and analytics.';
+message.innerHTML =
+    'We use cookies for improved functionality and analytics. <button class="btn btn--close-cookie">Got it</button>';
+// header.prepend(message);//add message before header
+header.append(message);//add message after header
+// header.append(message.cloneNode(true));//now message is in two places, because we cloned it. previous we just moved it, as it inserts to code only one time
+// header.before(message);
+// header.after(message);
+
+//delete elements
+document.querySelector('.btn--close-cookie')
+    .addEventListener('click', function (){
+      message.remove();
+    })
