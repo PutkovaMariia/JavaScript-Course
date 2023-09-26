b3c8caa15ae91ff1d8579c941406d432343deec5
PutkovaMariia <124445804+PutkovaMariia@users.noreply.github.com>
2023-09-26 17:18:42 +0200

    smooth scrolling

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -106,4 +106,30 @@ logo.classList.contains('c');//not 'includes' (contains only in DOM JS)
 logo.className = 'jonas';//DON"T USE THIS because it will rewrite all existing classes
 */
 
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 
+btnScrollTo.addEventListener('click', function (e){
+  const s1coords = section1.getBoundingClientRect();
+  // console.log(s1coords);
+  //
+  // console.log(e.target.getBoundingClientRect());
+  //
+  // console.log('current scroll (x/y)', window.pageXOffset, window.pageYOffset);
+  //
+  // console.log('height/width viewport', document.documentElement.clientHeight,
+  //     document.documentElement.clientWidth);
+
+  //scrolling
+  //window.scrollTo(s1coords.left + window.pageXOffset, s1coords.top + window.pageYOffset);
+
+  //old way to scroll
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behavior: 'smooth',
+  // })
+
+  //new way to scroll
+  section1.scrollIntoView({behavior: 'smooth'});
+})
