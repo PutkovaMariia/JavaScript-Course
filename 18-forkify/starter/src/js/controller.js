15f2f7a9d0eb8ef7fc6fbb4f90e8b5b55dccbf88
PutkovaMariia <124445804+PutkovaMariia@users.noreply.github.com>
2024-01-18 16:19:53 +0100

    Developing a DOM Updating Algorithm

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -21,6 +21,9 @@ const controlRecipes = async function () {
         if (!id) return;
         recipeView.renderSpinner();
 
+        //update results view to mark selected search results
+        resultsView.update(model.getSearchResultsPage());
+
         //loading recipe
         await model.loadRecipe(id);
 
@@ -69,7 +72,8 @@ const controlServings = function (newServings) {
     model.updateServings(newServings);
 
     //update the recipe view
-    recipeView.render(model.state.recipe);
+    //recipeView.render(model.state.recipe);
+    recipeView.update(model.state.recipe);
 };
 
 const init = function () {
