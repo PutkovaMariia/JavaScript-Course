736ce55818ab08a780075d52380b13a1cb8efde5
PutkovaMariia <124445804+PutkovaMariia@users.noreply.github.com>
2024-01-22 16:28:41 +0100

    Uploading a New Recipe - Part 1

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -4,6 +4,7 @@ import searchView from "./views/searchView";
 import resultsView from "./views/resultsView";
 import paginationView from "./views/paginationView";
 import bookmarksView from "./views/bookmarksView";
+import addRecipeView from "./views/addRecipeView";
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
@@ -97,6 +98,12 @@ const controlBookmarks= function (){
     bookmarksView.render(model.state.bookmarks);
 }
 
+const controlAddRecipe = function (newRecipe){
+    console.log(newRecipe);
+
+    //upload new recipe data
+}
+
 const init = function () {
     bookmarksView.addHandlerRender(controlBookmarks);
     recipeView.addHandlerRender(controlRecipes);
@@ -104,5 +111,6 @@ const init = function () {
     recipeView.addHandlerAddBookmark(controlAddBookmark);
     searchView.addHandlerSearch(controlSearchResults);
     paginationView.addHandlerClick(controlPagination);
+    addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 init();
